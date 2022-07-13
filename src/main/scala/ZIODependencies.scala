f2845e4c0d14b616cb58a8c8e03ee4c9ba676ab6
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-13 13:16:31 -0300

    Using subscribe service

--- src/main/scala/ZIODependencies.scala
+++ src/main/scala/ZIODependencies.scala
@@ -6,68 +6,6 @@ object ZIODependencies extends ZIOAppDefault:
 
    // app to subscribe users to newsletter
 
-   case class User(name: String, email: String)
-
-   class UserSubscription(
-       emailService: EmailService,
-       userDatabase: UserDatabase
-   ):
-      def subscribeUser(user: User): Task[Unit] =
-        for
-           _ <- emailService.email(user)
-           _ <- userDatabase.insert(user)
-        yield ()
-
-   object UserSubscription:
-      def create(
-        emailService: EmailService,
-        userDatabase: UserDatabase
-      ): UserSubscription =
-        UserSubscription(emailService, userDatabase)
-
-   class EmailService:
-      def email(user: User): Task[Unit] =
-        ZIO.succeed(s"You've just been subscribed, ${user.name}")
-   object EmailService:
-      def create(): EmailService =
-        EmailService()
-
-   class UserDatabase(connectionPool: ConnectionPool):
-      def insert(user: User): Task[Unit] =
-        for
-           connection <- connectionPool.get
-           _ <- connection.runQuery(s"Insert $user into database")
-        yield ()
-   object UserDatabase:
-      def create(connectionPool: ConnectionPool): UserDatabase =
-        UserDatabase(connectionPool)
-
-   class ConnectionPool(nConnections: Int):
-      def get: Task[Connection] =
-        ZIO.succeed(
-          println(s"Acquired connection out of $nConnections connections.")
-        )
-          *> ZIO.succeed(
-            Connection()
-          )
-   object ConnectionPool:
-      def create(nConnections: Int): ConnectionPool =
-        ConnectionPool(nConnections)
-
-   case class Connection():
-      def runQuery(query: String): Task[Unit] =
-        ZIO.succeed(println(s"Running query: $query"))
-
-   val subscriptionService: UIO[UserSubscription] =
-     ZIO.succeed( // Dependency injection
-       UserSubscription.create(
-         EmailService.create(),
-         UserDatabase.create(
-           ConnectionPool.create(8)
-         )
-       )
-     )
-
    def subscribe(user: User): Task[Unit] =
      for
         sub <- subscriptionService
@@ -80,8 +18,23 @@ object ZIODependencies extends ZIOAppDefault:
         _ <- subscribe(User("User2", "email2"))
      yield ()
 
+   // Alternative
+
+   def subscribe_v2(user: User): ZIO[UserSubscription, Throwable, Unit] =
+     for
+        // sub is of type ZIO[UserSubscription, Nothing, UserSubscriptionService
+        sub <- ZIO.service[UserSubscription]
+        _ <- sub.subscribeUser(user)
+     yield ()
+
+   val program_v2: ZIO[UserSubscription, Throwable, Unit] =
+     for
+        _ <- subscribe_v2(User("User1", "email1"))
+        _ <- subscribe_v2(User("User2", "email2"))
+     yield ()
+
    import scala.compiletime.uninitialized
    var later: Int = uninitialized
    later = 0
 
-   override def run = program
+   override def run = program  // program_v2.provide(ZLayer.fromZIO(subscriptionService))
