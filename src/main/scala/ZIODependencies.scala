9ba9b0427490d375ced11a93cce0562a62afdbd1
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-13 11:32:17 -0300

    Starting implementation

--- src/main/scala/ZIODependencies.scala
+++ src/main/scala/ZIODependencies.scala
@@ -12,22 +12,34 @@ object ZIODependencies extends ZIOAppDefault {
       emailService: EmailService,
       userDatabase: UserDatabase
   ):
-    def subscribeUser(user: User): Task[Unit] = ???
+     def subscribeUser(user: User): Task[Unit] =
+       for
+          _ <- emailService.email(user)
+          _ <- userDatabase.insert(user)
+       yield ()
 
   class EmailService:
-    def email(user: User): Task[Unit] = ???
+     def email(user: User): Task[Unit] =
+       ZIO.succeed(s"You've just been subscribed, ${user.name}")
 
   class UserDatabase(connectionPool: ConnectionPool):
-    def insert(user: User): Task[Unit] = ???
+     def insert(user: User): Task[Unit] =
+       for
+          connection <- connectionPool.get
+          _ <- connection.runQuery(s"Insert $user into database")
+       yield ()
 
   class ConnectionPool(nConnections: Int):
-    def get: Task[Connection] = ???
+     def get: Task[Connection] =
+       ZIO.succeed(println("Acquired connection.")) *> ZIO.succeed(Connection())
 
   case class Connection():
-    def runQuery(query: String): Task[Unit] = ???
+     def runQuery(query: String): Task[Unit] =
+       ZIO.succeed(println(s"Running query: $query"))
 
   import scala.compiletime.uninitialized
   var later: Int = uninitialized
+  later = 0
 
   override def run = ???
 
