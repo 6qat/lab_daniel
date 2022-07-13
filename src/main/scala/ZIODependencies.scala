6286779ba268573f30256dcdca058ae64cc9acd7
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-13 17:42:11 -0300

    Fihish ZLayer chapter

--- src/main/scala/ZIODependencies.scala
+++ src/main/scala/ZIODependencies.scala
@@ -2,6 +2,9 @@ package tc.lab.daniel
 
 import zio.*
 
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
 object ZIODependencies extends ZIOAppDefault:
 
     // app to subscribe users to newsletter
@@ -46,5 +49,27 @@ object ZIODependencies extends ZIOAppDefault:
       )
 
     val runnableProgram_v3 = program_v2.provide(userSubscriptionLayer_v3)
+
+    // pass through
+    val dbWithPoolLayer
+      : ZLayer[ConnectionPool, Nothing, ConnectionPool & UserDatabase] =
+      UserDatabase.live.passthrough
+
+    // service: take a dep and expose it as a value to further layers
+    val dbService: ZLayer[UserDatabase, Nothing, UserDatabase] =
+      ZLayer.service[UserDatabase]
+
+    // launch: creates a ZIO that uses the services and never finishes (good for server loops that never finishes)
+    val subscriptionLaunch: ZIO[EmailService & UserDatabase, Nothing, Nothing] =
+      UserSubscription.live.launch
+
+    // memoization (hidden feature)
+    // memoization is the default, unless you call .fresh on layer
+
+    // Services already provided: Clock, Random, System, Console
+    val getTime: UIO[Long] = Clock.currentTime(TimeUnit.SECONDS)
+    val randomValue: UIO[RuntimeFlags] = Random.nextInt
+    val sysVariable: IO[SecurityException, Option[String]] = System.env("HOME")
+    val printlnEffect: IO[IOException, Unit] = Console.printLine("Test...")
 
     override def run = runnableProgram_v3
