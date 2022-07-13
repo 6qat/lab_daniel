741858b823eef4b746918527e0a7b843ac0266e7
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-13 11:06:12 -0300

    Starting implementation

--- src/main/scala/ZIODependencies.scala
+++ src/main/scala/ZIODependencies.scala
@@ -6,12 +6,28 @@ object ZIODependencies extends ZIOAppDefault {
 
   // app to subscribe users to newsletter
 
-  case class user(name: String, email: String)
-  class UserSubscription
-  class EmailService
-  class userDatabase
+  case class User(name: String, email: String)
 
+  class UserSubscription(
+      emailService: EmailService,
+      userDatabase: UserDatabase
+  ):
+    def subscribeUser(user: User): Task[Unit] = ???
 
+  class EmailService:
+    def email(user: User): Task[Unit] = ???
+
+  class UserDatabase(connectionPool: ConnectionPool):
+    def insert(user: User): Task[Unit] = ???
+
+  class ConnectionPool(nConnections: Int):
+    def get: Task[Connection] = ???
+
+  case class Connection():
+    def runQuery(query: String): Task[Unit] = ???
+
+  import scala.compiletime.uninitialized
+  var later: Int = uninitialized
 
   override def run = ???
 
