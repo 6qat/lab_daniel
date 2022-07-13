cb3640964ecfcaa8acaf107002b3a9c1cb538163
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-13 19:54:53 -0300

    Refactorings

--- src/main/scala/ZIOEffects.scala
+++ src/main/scala/ZIOEffects.scala
@@ -2,16 +2,13 @@ package tc.lab.daniel
 
 import zio.*
 
-object ZioEffects {
+object ZIOEffects {
 
   val meaningOfLife: ZIO[Any, Nothing, Int] = ZIO.succeed(2)
   val aFailure: ZIO[Any, String, Nothing] = ZIO.fail("Something went wrong")
   val aSuspendedZIO: ZIO[Any, Throwable, Int] = ZIO.suspend(meaningOfLife)
   val improvedMeaningOfLife = meaningOfLife.map(_ * 2)
 
-
-  def main(args: Array[String]): Unit = {
-
-  }
+  def main(args: Array[String]): Unit = {}
 
 }
