867d8b0319f41cb6714bf8cb7e0e0c5ab5dc0550
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-13 11:36:31 -0300

    Indentation with new scala 3 syntax

--- src/main/scala/ZIODependencies.scala
+++ src/main/scala/ZIODependencies.scala
@@ -2,45 +2,45 @@ package tc.lab.daniel
 
 import zio.*
 
-object ZIODependencies extends ZIOAppDefault {
+object ZIODependencies extends ZIOAppDefault:
 
-  // app to subscribe users to newsletter
+   // app to subscribe users to newsletter
 
-  case class User(name: String, email: String)
+   case class User(name: String, email: String)
 
-  class UserSubscription(
-      emailService: EmailService,
-      userDatabase: UserDatabase
-  ):
-     def subscribeUser(user: User): Task[Unit] =
-       for
-          _ <- emailService.email(user)
-          _ <- userDatabase.insert(user)
-       yield ()
+   class UserSubscription(
+       emailService: EmailService,
+       userDatabase: UserDatabase
+   ):
+      def subscribeUser(user: User): Task[Unit] =
+        for
+           _ <- emailService.email(user)
+           _ <- userDatabase.insert(user)
+        yield ()
 
-  class EmailService:
-     def email(user: User): Task[Unit] =
-       ZIO.succeed(s"You've just been subscribed, ${user.name}")
+   class EmailService:
+      def email(user: User): Task[Unit] =
+        ZIO.succeed(s"You've just been subscribed, ${user.name}")
 
-  class UserDatabase(connectionPool: ConnectionPool):
-     def insert(user: User): Task[Unit] =
-       for
-          connection <- connectionPool.get
-          _ <- connection.runQuery(s"Insert $user into database")
-       yield ()
+   class UserDatabase(connectionPool: ConnectionPool):
+      def insert(user: User): Task[Unit] =
+        for
+           connection <- connectionPool.get
+           _ <- connection.runQuery(s"Insert $user into database")
+        yield ()
 
-  class ConnectionPool(nConnections: Int):
-     def get: Task[Connection] =
-       ZIO.succeed(println("Acquired connection.")) *> ZIO.succeed(Connection())
+   class ConnectionPool(nConnections: Int):
+      def get: Task[Connection] =
+        ZIO.succeed(println("Acquired connection.")) *> ZIO.succeed(
+          Connection()
+        )
 
-  case class Connection():
-     def runQuery(query: String): Task[Unit] =
-       ZIO.succeed(println(s"Running query: $query"))
+   case class Connection():
+      def runQuery(query: String): Task[Unit] =
+        ZIO.succeed(println(s"Running query: $query"))
 
-  import scala.compiletime.uninitialized
-  var later: Int = uninitialized
-  later = 0
+   import scala.compiletime.uninitialized
+   var later: Int = uninitialized
+   later = 0
 
-  override def run = ???
-
-}
+   override def run = ???
