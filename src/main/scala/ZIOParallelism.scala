fb73cd9f0118e4902ff0695cd297b518df804bc0
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-20 16:08:29 -0300

    Starting parallelism chapter

--- src/main/scala/ZIOParallelism.scala
+++ src/main/scala/ZIOParallelism.scala
@@ -1,3 +1,28 @@
 package tc.lab.daniel
+import zio.*
 
-object ZIOParallelism {}
+object ZIOParallelism extends ZIOAppDefault {
+
+  def myZipPar[R, E, A, B](
+    zioa: ZIO[R, E, A],
+    ziob: ZIO[R, E, B]
+  ): ZIO[R, E, (A, B)] =
+      val exits: ZIO[R, Nothing, (Exit[E, A], Exit[E, B])] = for
+          fiba <- zioa.fork
+          fibb <- ziob.fork
+          exita <- fiba.await
+          exitb <- exita match
+              case Exit.Success(_) => fibb.await
+              case Exit.Failure(_) => fibb.interrupt
+      yield (exita, exitb)
+
+      exits.flatMap {
+        case (Exit.Success(a), Exit.Success(b))     => ZIO.succeed((a, b))
+        case (Exit.Success(_), Exit.Failure(cause)) => ZIO.failCause(cause)
+        case (Exit.Failure(cause), Exit.Success(_)) => ZIO.failCause(cause)
+        case (Exit.Failure(causea), Exit.Failure(causeb)) =>
+          ZIO.failCause(causea && causeb)
+      }
+
+  def run = ???
+}
