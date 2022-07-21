bea1f2eeb643d2ad0ab41d5bdd401fc5662c57b2
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-07-21 14:09:23 -0300

    Playing with resources

--- src/main/scala/ZIOResources.scala
+++ src/main/scala/ZIOResources.scala
@@ -15,6 +15,38 @@ object ZIOResources extends ZIOAppDefault {
 
   val anAttemptWith2Finalizers =
     annAttemptWithFinalizer.ensuring(ZIO.succeed("finalizer 2").debugThread)
+    // onInterrupt, onError, onDone, onExit
 
-  def run = anAttemptWith2Finalizers
+  // resource lifecycle
+  class Connection(url: String):
+      def open() = ZIO.succeed(s"Opening connection to $url").debugThread
+      def close() = ZIO.succeed(s"Closing connection to $url").debugThread
+
+  object Connection:
+      def create(url: String): ZIO[Any, Nothing, Connection] =
+        ZIO.succeed(new Connection(url))
+
+  val fetchUrl =
+    for
+        conn <- Connection.create("rockthejvm.com")
+        fiber <- (conn.open() *> Clock.sleep(300.seconds)).fork
+        _ <- Clock.sleep(1.second) *> ZIO
+          .succeed("interrupting")
+          .debugThread *> fiber.interrupt
+        _ <- fiber.join
+    yield ()
+
+  val correctFetchUrl =
+    for
+        conn <- Connection.create("rockthejvm.com")
+        fiber <- (conn.open() *> Clock.sleep(300.seconds))
+          .ensuring(conn.close())
+          .fork
+        _ <- Clock.sleep(1.second) *> ZIO
+          .succeed("interrupting")
+          .debugThread *> fiber.interrupt
+        _ <- fiber.join
+    yield ()
+
+  def run = correctFetchUrl
 }
